ae4b2228d62e86a2ef180e06d446d5e4b12b8a0a
serenasophy <serenasophy8@gmail.com>
2025-05-05 12:02:39 +0300

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,20 @@
+class Vehicle:
+    def move(self):
+        print("This vehicle moves...")
+
+class Car(Vehicle):
+    def move(self):
+        print("Driving 🚗")
+
+class Plane(Vehicle):
+    def move(self):
+        print("Flying ✈️")
+
+class Boat(Vehicle):
+    def move(self):
+        print("Sailing 🚢")
+
+vehicles = [Car(), Plane(), Boat()]
+
+for v in vehicles:
+    v.move()
